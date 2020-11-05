cbf52007e104561c97c529107145e2049aeb78a0
jwosborn <osbornjeremyw@yahoo.com>
2020-11-05 13:28:11 -0500

    some initial scaffolding

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"accumulate"
+	"hamming"
+	"leap"
+	"nucleotide"
+	"proverb"
+	"raindrops"
+	"rna"
+	"roman"
+	"space_age"
+	"triange"
+	"two_fer"
+)
+
+func main() {
+
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module exserver
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/golang/mock v1.4.4 // indirect
+	github.com/gorilla/mux v1.8.0
+)
